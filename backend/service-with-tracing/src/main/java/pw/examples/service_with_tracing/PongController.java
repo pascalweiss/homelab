efeb2cd9209719bead6f4595a61a4753a6ed3826
pascalweiss <mail@pascal-weiss.de>
2024-11-08 16:08:19 +0100

    request chain for tracing-app

--- backend/service-with-tracing/src/main/java/pw/examples/service_with_tracing/PongController.java
+++ backend/service-with-tracing/src/main/java/pw/examples/service_with_tracing/PongController.java
@@ -1,5 +1,6 @@
 package pw.examples.service_with_tracing;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -7,12 +8,20 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class PongController {
 
+    @Value("${pw.homelab.pong.port}")
+    private String pongPort;
+
     RestTemplate restTemplate = new RestTemplate();
 
     @GetMapping("/ping")
-    public String ping() {
-        restTemplate.getForObject("http://localhost:8080/pong", String.class);
-        return "ping";
+    public String receiveFromUser() {
+        var response = restTemplate.getForObject("http://localhost:%s/pong".formatted(pongPort), String.class);
+        return "ping " + response;
+    }
+
+    @GetMapping("/pong")
+    public String receiveRand() {
+        return "pong";
     }
 
 }
