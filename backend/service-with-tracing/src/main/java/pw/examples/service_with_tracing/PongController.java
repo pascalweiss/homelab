417c36444ab9786e7c18ba86c105912dca891e90
pascalweiss <mail@pascal-weiss.de>
2024-11-10 11:12:00 +0100

    use auto-configured RestTemplate from config to keep traces over http requests + some polishing

--- backend/service-with-tracing/src/main/java/pw/examples/service_with_tracing/PongController.java
+++ backend/service-with-tracing/src/main/java/pw/examples/service_with_tracing/PongController.java
@@ -1,5 +1,6 @@
 package pw.examples.service_with_tracing;
 
+import io.opentelemetry.api.trace.Span;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,20 +17,34 @@ public class PongController {
     private String pongPort;
 
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
+    /**
+     * Receives a ping from the user and requests a pong from the pong service.
+     */
     @GetMapping("/ping")
     public String receiveFromUser() {
-        log.info("Received ping, requesting pong");
-        var response = restTemplate.getForObject("http://localhost:%s/pong".formatted(pongPort), String.class);
-        log.info("Received pong, returning ping pong");
+        logInfo("received ping, sending pong");
+        var response = restTemplate.getForObject(getPongUrl(), String.class);
         return "ping " + response;
     }
 
+    /**
+     * Receives a pong from the ping service.
+     */
     @GetMapping("/pong")
     public String receiveRand() {
-        log.info("Received pong, returning pong");
+        logInfo("Received pong, returning pong");
         return "pong";
+    }
+
+    private String getPongUrl() {
+        return "http://localhost:%s/pong".formatted(pongPort);
+    }
+
+    private void logInfo(String msg) {
+        var traceId = Span.current().getSpanContext().getTraceId();
+        log.info("traceId: {}, msg: {}", traceId, msg);
     }
 
 }
