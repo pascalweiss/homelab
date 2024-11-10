9080718c013cd06f4cad9f99b996555020a9fae7
pascalweiss <mail@pascal-weiss.de>
2024-11-10 11:12:34 +0100

    enable script to set service name

--- backend/service-with-tracing/run/runWithPort.sh
+++ backend/service-with-tracing/run/runWithPort.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# read in the port number and throw an error, when no param is given. Please use a common output for the error message.
-if [ -z "$1" ]; then
-  echo "Use: gradle bootRun <port>"
+# read in the port number and service name, and throw an error when no params are given. Please use a common output for the error message.
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Use: gradle bootRun <port> <service_name>"
   exit 1
 fi
 
-
-SERVER_PORT="$1" gradle bootRun
+SERVER_PORT="$1" SERVICE_NAME="$2" gradle bootRun
