b4ad96a56bd8af814e1c99eb3ab0ffa6241c7f6a
pascalweiss <mail@pascal-weiss.de>
2024-11-11 08:27:33 +0100

    removed service, which will become submodule next

--- backend/service-with-tracing/run/runWithPort.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# read in the port number and service name, and throw an error when no params are given. Please use a common output for the error message.
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Use: gradle bootRun <port> <service_name>"
-  exit 1
-fi
-
-SERVER_PORT="$1" SERVICE_NAME="$2" gradle bootRun
